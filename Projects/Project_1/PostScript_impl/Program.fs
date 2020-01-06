1aabccfe5ceaeed634eb72eb9ae18a340bf372dc
Andreas <Andreas@10.16.168.88>
2020-01-06 15:59:12 +0100

    Update gsgen & design, so design uses module.

--- Projects/Project_1/PostScript_impl/Program.fs
+++ Projects/Project_1/PostScript_impl/Program.fs
@@ -1,58 +1,90 @@
-type Tree<'x> = Node of 'x * Tree<'x> list;;
-
-type PageSetup = (float * float);;
+open TreeDesign
+open System
+open System.IO
+open System.Diagnostics
 
 let rec findLargestX (Node((_, x), subtree)) : float =
     match subtree with
     | [] -> x
     | _ -> List.max (List.map findLargestX subtree)
 
-let rec generateImpl (Node((label, x), subtree)) (y : float) (maxX : float) : string =
+let toIntString (x : float) =
+    string(int(x))
+
+let rec generateImpl (Node((label, _), subtree)) (x : float) (y : float) (maxX : float) : string =
     String.concat "" [
-        string(int(x / maxX * 800.0));
+        toIntString(x);
         " ";
-        string(int(y));
+        toIntString(y);
         " moveto\n(";
         label;
         ") dup stringwidth pop 2 div neg 0 rmoveto show";
-        (String.concat "\n" (List.map (fun (Node((label2, x2), subtree2)) -> String.concat "" [
+        (String.concat "\n" (List.map (fun (Node((label2, dx), subtree2)) -> String.concat "" [
             "\nnewpath\n";
-            string(int(x / maxX * 800.0));
+            toIntString(x);
             " ";
-            string(int(y - 16.0));
+            toIntString(y - 8.0);
             " moveto\n";
-            string(int(x / maxX * 800.0));
+            toIntString(x);
             " ";
-            string(int(y - 25.0));
+            toIntString(y - 25.0);
             " lineto\n";
-            string(int(x2 / maxX * 800.0));
+            toIntString(x + dx / maxX);
             " ";
-            string(int(y - 25.0));
+            toIntString(y - 25.0);
             " lineto\n";
-            string(int(x2 / maxX * 800.0));
+            toIntString(x + dx / maxX);
             " ";
-            string(int(y - 40.0));
+            toIntString(y - 40.0);
             " lineto\nstroke\n";
-            (generateImpl (Node((label2, x2), subtree2)) (y - 50.0) maxX)
+            (generateImpl (Node((label2, dx), subtree2)) (x + dx / maxX) (y - 50.0) maxX)
         ]) subtree))
     ]
 
 let generate (designResult : Tree<string*float>) : string =
     String.concat "" [
         "%!\n<</PageSize[1000 1400]/ImagingBBox null>> setpagedevice\n/Times-Roman findfont 10 scalefont setfont\n1 1 scale\n";
-        (generateImpl designResult (842.0 - 30.0) (findLargestX designResult));
+        (generateImpl designResult 500.0 1340.0 ((findLargestX designResult + 20.0) / 1000.0));
         "\nshowpage"
     ]
 
+let psToPdfFile (psContents : string) =
+    File.WriteAllText ("tmp_file.ps", psContents) |> ignore
+    Process.Start ("../../../ghostscript/gswin64c.exe", "tmp_file.ps") |> ignore
+
 [<EntryPoint>]
 let main argv =
-    let tree = Node(("hello", 10.0), [
+    (*let tree = Node(("hello", 10.0), [
         Node(("world", 5.0), [
             Node(("abc", 2.0), []);
             Node(("def", 8.0), [])]
         );
         Node(("f#", 15.0), [])
-    ])
+    ])*)
+    let tree = TreeDesign.design (Node("hello", [
+        Node("world", [
+            Node("abc", []);
+            Node("def", [])]
+        );
+        Node("f#", [
+            Node("hello", [
+                Node("world", [
+                    Node("abc", []);
+                    Node("def", [
+                        Node("hello", [
+                            Node("world", [
+                                Node("abc", []);
+                                Node("def", [])]
+                            );
+                            Node("f#", [])
+                        ])
+                    ])]
+                );
+                Node("f#", [])
+            ])
+        ])
+    ]))
     let result = generate tree
-    printfn "%s" result
+    //printfn "%s" result
+    psToPdfFile result
     0 // return an integer exit code
--- Projects/Project_1/Trees_impl/Program.fs
+++ Projects/Project_1/Trees_impl/Program.fs
@@ -1,50 +1,48 @@
-﻿open System
+﻿namespace TreeDesign
+open System
 
-type Tree<'x> = Node of 'x * Tree<'x> list;;
+type Tree<'x> = Node of 'x * Tree<'x> list
 
-let movetree (Node((label, x), subtrees), dx:float) = Node((label, x + dx), subtrees);;
+module TreeDesign =
+    
 
-type Extent = (float * float) list;;
+    let movetree (Node((label, x), subtrees), dx:float) = Node((label, x + dx), subtrees)
 
-let moveextent (e:Extent, x) = List.map(fun (p, q) -> (p + x, q + x)) e
+    type Extent = (float * float) list
 
-let rec merge(ps, qs) = match (ps, qs) with
-                        | ([], qs) -> qs
-                        | (ps, []) -> ps
-                        | ((p, _)::ps, (_, q)::qs) -> (p, q)::merge(ps, qs);;
+    let moveextent (e:Extent, x) = List.map(fun (p, q) -> (p + x, q + x)) e
 
-let mergelist es = List.fold(fun x y -> merge(x, y)) [] es;;
+    let rec merge(ps, qs) = match (ps, qs) with
+                            | ([], qs) -> qs
+                            | (ps, []) -> ps
+                            | ((p, _)::ps, (_, q)::qs) -> (p, q)::merge(ps, qs)
 
-let rec fit aa bb = match (aa, bb) with
-                    | ((_, p)::ps, (q, _)::qs) -> Math.Max(fit ps qs, p - q + 1.0)
-                    | (_, _) -> 0.0;;
+    let mergelist es = List.fold(fun x y -> merge(x, y)) [] es
 
-let rec fitlistl es = let rec fitlistll acc bb = match bb with
+    let rec fit aa bb = match (aa, bb) with
+                        | ((_, p)::ps, (q, _)::qs) -> Math.Max(fit ps qs, p - q + 1.0)
+                        | (_, _) -> 0.0
+
+    let rec fitlistl es = let rec fitlistll acc bb = match bb with
+                                                     | [] -> []
+                                                     | (b::btail)  -> let x = fit acc b
+                                                                      x::(fitlistll (merge (acc, moveextent (b, x))) btail)
+                          fitlistll [] es
+
+    let fitlistr es = let rec fitlistrr acc bb = match bb with
                                                  | [] -> []
-                                                 | (b::btail)  -> let x = fit acc b
-                                                                  x::(fitlistll (merge (acc, moveextent (b, x))) btail)
-                      fitlistll [] es;;
+                                                 | (b::btail) -> let x = -(fit b acc)
+                                                                 x::(fitlistrr (merge (moveextent (b, x), acc)) btail)
+                      List.rev (fitlistrr [] (List.rev es))
 
-let fitlistr es = let rec fitlistrr acc bb = match bb with
-                                             | [] -> []
-                                             | (b::btail) -> let x = -(fit b acc)
-                                                             x::(fitlistrr (merge (moveextent (b, x), acc)) btail)
-                  List.rev (fitlistrr [] (List.rev es));;
+    let mean (x,y) = (x + y) / 2.0
+    let fitlist es = List.map mean (List.zip (fitlistl es) (fitlistr es))
 
-let mean (x,y) = (x + y) / 2.0;;
-let fitlist es = List.map mean (List.zip (fitlistl es) (fitlistr es));;
-
-let design tree = let rec designn (Node(label, subtrees)) = let (trees, extents) = List.unzip (List.map designn subtrees)
-                                                            let positions = fitlist extents
-                                                            let ptrees = List.map movetree (List.zip trees positions)
-                                                            let pextents = List.map moveextent (List.zip extents positions)
-                                                            let resultextent = (0.0, 0.0) :: mergelist pextents
-                                                            let resulttree = Node((label, 0.0), ptrees)
-                                                            (resulttree, resultextent)
-                  fst (designn tree);;
-
-
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let design tree = let rec designn (Node(label, subtrees)) = let (trees, extents) = List.unzip (List.map designn subtrees)
+                                                                let positions = fitlist extents
+                                                                let ptrees = List.map movetree (List.zip trees positions)
+                                                                let pextents = List.map moveextent (List.zip extents positions)
+                                                                let resultextent = (0.0, 0.0) :: mergelist pextents
+                                                                let resulttree = Node((label, 0.0), ptrees)
+                                                                (resulttree, resultextent)
+                      fst (designn tree)
