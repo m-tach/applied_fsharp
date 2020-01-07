b498401a8456ef8f00c6c8bc7885e6a8d72de323
TheAIBot <andreasuranus@hotmail.com>
2020-01-07 15:26:57 +0100

    can now generate random trees

--- Projects/Project_1/Benchmarks/Program.fs
+++ Projects/Project_1/Benchmarks/Program.fs
@@ -6,20 +6,23 @@ open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Running
 
 let rec randomTree n (rnd:Random) = match n with
-                                    | 0 -> Node("Testing" + rnd.Next(0, 100).ToString(), [])
-                                    | _ -> let subTreeCount = rnd.Next(0, Math.Min(n, 10))
-                                           Node("Testing" + rnd.Next(0, 100).ToString(), List.map(fun _ -> randomTree (n - 1) rnd) [1..subTreeCount])
-
+                                         | 0 -> Node("Test", [])
+                                         | _ -> let subTreeCount = rnd.Next(1, Math.Min(10, n + 1))
+                                                let subtrees = List.fold(fun (left, nodes) _ -> let allocatedNodes = rnd.Next(left)
+                                                                                                (left - allocatedNodes, (randomTree allocatedNodes rnd)::nodes)) (n - subTreeCount, []) [1..(subTreeCount - 1)]
+                                                Node("Test", (randomTree (fst subtrees) rnd)::(snd subtrees))
+                                       
+let makeRandomTree n = randomTree (n - 1) (new Random(334234))
 
 type BenchmarkPostscriptGen () =
     let mutable tree : Tree<string * float> = Node(("", 0.0), [])
 
-    [<Params (10)>] 
+    [<Params (100, 500, 1000, 5000, 10000)>]
     member val public TreeSize = 0 with get, set
 
     [<GlobalSetup>]
     member self.GlobalSetupData() =
-        tree <- TreeDesign.design (randomTree self.TreeSize (new Random()))
+        tree <- TreeDesign.design (makeRandomTree self.TreeSize)
 
     [<Benchmark>]
     member self.StringConcat () = PostScriptGen.generateConcat tree
--- Projects/Project_1/Runner/Program.fs
+++ Projects/Project_1/Runner/Program.fs
@@ -24,57 +24,20 @@ let ex6 = P([
                 PrintLn(Access(AVar("x")))
             ])
 
-let randomTree = Node("hello", [
-    Node("world", [
-        Node("abc", []);
-        Node("def", [])]
-    );
-    Node("f#", [
-        Node("hello", [
-            Node("world", [
-                Node("abc", []);
-                Node("def", [
-                    Node("hello", [
-                        Node("world", [
-                            Node("abc", []);
-                            Node("def", [])]
-                        );
-                        Node("f#", [])
-                    ])
-                ]);Node("def", [
-                    Node("hello", [
-                        Node("world", [
-                            Node("abc", []);
-                            Node("def", [])]
-                        );
-                        Node("f#", [])
-                    ])
-                ]);Node("def", [
-                    Node("hello", [
-                        Node("world", [
-                            Node("abc", []);
-                            Node("def", [])]
-                        );
-                        Node("f#", [])
-                    ])
-                ]);Node("def", [
-                    Node("hello", [
-                        Node("world", [
-                            Node("abc", []);
-                            Node("def", [])]
-                        );
-                        Node("f#", [])
-                    ])
-                ])]
-            );
-            Node("f#", [])
-        ])
-    ])
-])
+let rec randomTree n (rnd:Random) = match n with
+                                         | 0 -> Node("Test", [])
+                                         | _ -> let subTreeCount = rnd.Next(1, Math.Min(10, n + 1))
+                                                let subtrees = List.fold(fun (left, nodes) _ -> let allocatedNodes = rnd.Next(left)
+                                                                                                (left - allocatedNodes, (randomTree allocatedNodes rnd)::nodes)) (n - subTreeCount, []) [1..(subTreeCount - 1)]
+                                                Node("Test", (randomTree (fst subtrees) rnd)::(snd subtrees))
+                                       
+let makeRandomTree n = randomTree (n - 1) (new Random(334234))
+
+
 
 [<EntryPoint>]
 let main argv =
     //let result = PostScriptGen.generate (TreeDesign.design randomTree)
-    let result = PostScriptGen.generate (TreeDesign.design (ASTConverter.programToTree ex6))
+    let result = PostScriptGen.generate (TreeDesign.design (makeRandomTree 100))
     PostScriptGen.psToPdfFile result
     0 // return an integer exit code
