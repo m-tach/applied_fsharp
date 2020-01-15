0536dda7d1ea023a9aef4ad3f48ba1412a05b397
mariya <mariya.tacheva@gmx.com>
2020-01-15 15:00:48 +0100

    PRINTC now prints on the same line

--- Projects/Project_2/VirtualMachine/Library.fs
+++ Projects/Project_2/VirtualMachine/Library.fs
@@ -120,7 +120,7 @@ let rec execcode(p: int[], s: int[], iargs: int[]) trace =
         sp := !sp-p.[!pc+1]; bp := s.[!sp-1]; pc := s.[!sp-2];  sp := !sp-2 
         s.[!sp] <- res            
     | i when i=CODEPRINTI -> System.Console.WriteLine(string s.[!sp] + " ") ; pc := !pc+1  
-    | i when i=CODEPRINTC -> System.Console.WriteLine((char)(s.[!sp])); pc := !pc+1 
+    | i when i=CODEPRINTC -> System.Console.Write((char)(s.[!sp])); pc := !pc+1 
     | i when i=CODELDARGS -> let k = ref 0
                              while !k< iargs.Length do  // Push commandline arguments
                                 sp:= !sp+1; s.[!sp] <- iargs.[!k] ; k:= !k + 1 
