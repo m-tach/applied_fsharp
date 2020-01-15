581c6b511165b9f11eb07f572197148c1c2adeab
mariya <mariya.tacheva@gmx.com>
2020-01-15 14:24:57 +0100

    using VirtualMachine.fsproj instead of .dll

--- Projects/Project_2/Compiler/Util.fs
+++ Projects/Project_2/Compiler/Util.fs
@@ -58,7 +58,7 @@ module CompilerUtil =
    let go p = run(code2ints(CodeGeneration.CP p))
 
 /// goOpt p compile and runs an abstract syntax for a program showing a program trace  
-   let goTrace p = VirtualMachine.runTrace(code2ints(CodeGeneration.CP p))
+   let goTrace p = runTrace(code2ints(CodeGeneration.CP p))
 
 /// exec filename parses, type checks, compiles and runs a program in a file
    let exec filename =  printfn "\nParse, typecheck, compilation and execution of %s:" filename 
