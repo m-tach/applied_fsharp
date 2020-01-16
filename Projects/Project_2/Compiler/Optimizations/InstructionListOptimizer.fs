b45b22355a290db85508e6932893d6216a337922
TheAIBot <andreasuranus@hotmail.com>
2020-01-16 07:05:11 +0100

    instr list optimizer now only returns when it can't optimize the list anymore

--- Projects/Project_2/Compiler/Optimizations/InstructionListOptimizer.fs
+++ Projects/Project_2/Compiler/Optimizations/InstructionListOptimizer.fs
@@ -4,14 +4,21 @@ open System
 open Machine
 
 module InstructionListAnalyzer =
-    let rec public optimizeInstrList instrs =
-        match instrs with
-        //Dup
-        | DUP :: INCSP a :: rest when a < 0 -> INCSP (a - 1) :: optimizeInstrList rest
-        | DUP :: RET a :: rest when a < 0 -> RET (a - 1) :: optimizeInstrList rest
-        //Incsp
-        | INCSP a :: INCSP b :: rest -> INCSP (a + b) :: optimizeInstrList rest
-        | GOTO a :: Label b :: rest when a = b -> optimizeInstrList rest
-        | CALL(m, a):: RET n :: rest -> RET n :: TCALL(m, n, a) :: optimizeInstrList rest
-        | a :: rest -> a :: optimizeInstrList rest
-        | [] -> []
+    let rec public optimizeInstrList allInstrs =
+        let rec optimizeInstrListInternal instrs =
+            match instrs with
+            //Dup
+            | DUP :: INCSP a :: rest when a < 0 -> INCSP (a - 1) :: optimizeInstrList rest
+            | DUP :: RET a :: rest when a < 0 -> RET (a - 1) :: optimizeInstrList rest
+            //Incsp
+            | INCSP a :: INCSP b :: rest -> INCSP (a + b) :: optimizeInstrList rest
+            | GOTO a :: Label b :: rest when a = b -> optimizeInstrList rest
+            | GOTO a :: GOTO b :: rest when a = b -> printfn "%s" "adaw" 
+                                                     GOTO a :: optimizeInstrList rest
+            | CALL(m, a):: RET n :: rest -> RET n :: TCALL(m, n, a) :: optimizeInstrList rest
+            | a :: rest -> a :: optimizeInstrList rest
+            | [] -> []
+        let optiInstrs = optimizeInstrListInternal allInstrs
+        if optiInstrs.Length <> allInstrs.Length
+        then optimizeInstrList optiInstrs
+        else optiInstrs
