f6236226334f34febca56b237b2306ba30b5ba5f
TheAIBot <andreasuranus@hotmail.com>
2020-01-17 12:42:14 +0100

    upade test program while making this feature

--- Projects/Project_2/Runner/Program.fs
+++ Projects/Project_2/Runner/Program.fs
@@ -11,17 +11,18 @@ open CompilerUtil
 open Visualizer
 
 let runInstrs instrs =
-    Array.toList(VirtualMachine.execcode(List.toArray (Machine.code2ints instrs), (Array.zeroCreate 10000), (Array.zeroCreate 0)) false)
+    Array.toList(VirtualMachine.execcode(List.toArray (Machine.code2ints instrs), (Array.zeroCreate 20000), (Array.zeroCreate 0)) false)
 
 let runAST ast =
     let instrs = CP ast
     let optiInstrs = optimize instrs
-    printfn "%A" (runInstrs instrs)
-    printfn "%A" (runInstrs optiInstrs)
+    printfn "%s" (String.concat ", " (List.map(fun x -> string(x)) (runInstrs instrs)))
+    printfn "%s" (String.concat ", " (List.map(fun x -> string(x)) (runInstrs optiInstrs)))
+    //VirtualMachine.runTrace (Machine.code2ints optiInstrs)
 
 [<EntryPoint>]
 let main argv =
-    let ex0Tree = parseFromFile "../programs/partition.gc"
+    let ex0Tree = parseFromFile "../programs/QuickSortV2.gc"
     let instrs = CP ex0Tree
     let optiInstrs = optimize instrs
     
