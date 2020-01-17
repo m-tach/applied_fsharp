b9dbc03f7eec2c632f51dabeeccbbc3a3ca174a0
TheAIBot <andreasuranus@hotmail.com>
2020-01-17 14:14:45 +0100

    awd

--- Projects/Project_2/Runner/Program.fs
+++ Projects/Project_2/Runner/Program.fs
@@ -11,13 +11,13 @@ open CompilerUtil
 open Visualizer
 
 let runInstrs instrs =
-    Array.toList(VirtualMachine.execcode(List.toArray (Machine.code2ints instrs), (Array.zeroCreate 20000), (Array.zeroCreate 0)) false)
+    Array.toList(VirtualMachine.execcode(List.toArray (Machine.code2ints instrs), (Array.zeroCreate 10000), (Array.zeroCreate 0)) false)
 
 let runAST ast =
     let instrs = CP ast
     let optiInstrs = optimize instrs
-    printfn "%s" (String.concat ", " (List.map(fun x -> string(x)) (runInstrs instrs)))
-    printfn "%s" (String.concat ", " (List.map(fun x -> string(x)) (runInstrs optiInstrs)))
+    printfn "%A" (runInstrs instrs)
+    printfn "%A" (runInstrs optiInstrs)
     //VirtualMachine.runTrace (Machine.code2ints optiInstrs)
 
 [<EntryPoint>]
@@ -26,7 +26,7 @@ let main argv =
     let instrs = CP ex0Tree
     let optiInstrs = optimize instrs
     
-    //printfn "not optimized\n%s" (instrsToString instrs)
+    printfn "not optimized\n%s" (instrsToString instrs)
     printfn "\n\noptimized\n%s" (instrsToString optiInstrs)
     printfn ""
     printfn "no opti:   %s" (instrs.Length.ToString())
