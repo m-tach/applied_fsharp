16d64f5439021e03c65fdfdcb7c196b6e5acadaa
Andreas <bloxxer9001@gmail.com>
2020-01-17 12:59:24 +0100

    Added test for data type checking & typecheck of the tests.

--- Projects/Project_2/Tests/PreincrementDecrementTests.fs
+++ Projects/Project_2/Tests/PreincrementDecrementTests.fs
@@ -18,12 +18,14 @@ type PreincrementDecrementTests() =
     [<TestMethod>]
     member this.TestPreincrement () =
         let p = parseFromFile "programs/Preincrement1.gc"
+        tcP p
         Assert.IsTrue (List.forall2 (=) (List.ofArray (goTrace p)) [2]) ;
 
     // Define i = 5, j = 0. Decrements j (thus -1), and loops ++i until i becomes 10.
     [<TestMethod>]
     member this.TestPreincPredecLoop () =
         let p = parseFromFile "programs/PreincDecLoop.gc" ;
+        tcP p
         Assert.IsTrue (List.forall2 (=) (List.ofArray (goTrace p)) [10; -1]) ;
 
     //expect an error because only integer values supports preincrement and predecrement
@@ -35,4 +37,13 @@ type PreincrementDecrementTests() =
     [<TestMethod>]
     member this.TestPredecrementLoop () =
         let p = parseFromFile "programs/PredecrementLoop.gc" ;
+        tcP p
         Assert.IsTrue (List.forall2 (=) (List.ofArray (goTrace p)) [0]) ;
+
+    // Test of int (vars), int[] (arrays) and int^ (pointer derefs) on preincrement and predecrement
+    // The pointer updates i, so it becomes 7. Stack positions to care about are 0, 3, 4.
+    [<TestMethod>]
+    member this.TestPreincDecAccessTypes () =
+        let p = parseFromFile "programs/PreincDecAccessTypes.gc" ;
+        tcP p
+        Assert.IsTrue (List.forall2 (=) (List.ofArray (goTrace p)) [7; 2; 2; -1; 0]) ;
