c674f733f1a210da2c763b2971e6c8c957ff338c
TheAIBot <andreasuranus@hotmail.com>
2020-01-20 12:41:59 +0100

    more tests

--- Projects/Project_2/Tests/OperatorTests.fs
+++ Projects/Project_2/Tests/OperatorTests.fs
@@ -24,6 +24,7 @@ type TestOperator () =
         let program = String.Format("begin x: {0}, y: {0}, z: {0}; z := x {1} y end", typ, oper)
         try
             let ast = parseString program
+            tcP ast
             runInstrs (CP ast) |> ignore
         with
         | e -> failwith (String.Format("Program:\n{0}\n\nFailed with: {1}\n\nStacktrace:\n", program, e.Message, e.StackTrace)) 
@@ -32,6 +33,7 @@ type TestOperator () =
         let program = String.Format("begin x: {0}, y: {0}, z: bool; z := x {1} y end", typ, oper)
         try
             let ast = parseString program
+            tcP ast
             runInstrs (CP ast) |> ignore   
         with
         | e -> failwith (String.Format("Program:\n{0}\n\nFailed with: {1}\n\nStacktrace:\n", program, e.Message, e.StackTrace)) 
@@ -39,6 +41,7 @@ type TestOperator () =
         let program = String.Format("begin x: {0}, z: bool; z := {1} x end", typ, oper)
         try
             let ast = parseString program
+            tcP ast
             runInstrs (CP ast) |> ignore 
         with
         | e -> failwith (String.Format("Program:\n{0}\n\nFailed with: {1}\n\nStacktrace:\n", program, e.Message, e.StackTrace)) 
@@ -46,6 +49,7 @@ type TestOperator () =
         let program = String.Format("begin x: {0}, z: {1}; z := {2} x end", typ1, typ2, oper)
         try
             let ast = parseString program
+            tcP ast
             runInstrs (CP ast) |> ignore
         with
         | e -> failwith (String.Format("Program:\n{0}\n\nFailed with: {1}\n\nStacktrace:\n", program, e.Message, e.StackTrace))       
@@ -53,6 +57,7 @@ type TestOperator () =
         let program = String.Format("begin x: {0}, z: bool; z := x {1} end", typ, oper)
         try
             let ast = parseString program
+            tcP ast
             runInstrs (CP ast) |> ignore 
         with
         | e -> failwith (String.Format("Program:\n{0}\n\nFailed with: {1}\n\nStacktrace:\n", program, e.Message, e.StackTrace))      
@@ -66,22 +71,109 @@ type TestOperator () =
     [<TestMethod>]
     member this.IntDiv () = verifyBIExpr "int" "/"
     [<TestMethod>]
-    member this.IntEq () = verifyBIExpr "int" "="
+    member this.IntEq () = verifyBIExprToBool "int" "="
     [<TestMethod>]
-    member this.IntLess () = verifyBIExpr "int" "<"
+    member this.IntLess () = verifyBIExprToBool "int" "<"
     [<TestMethod>]
-    member this.IntLeq () = verifyBIExpr "int" "<="
+    member this.IntLeq () = verifyBIExprToBool "int" "<="
     [<TestMethod>]
-    member this.IntGeq () = verifyBIExpr "int" ">="
+    member this.IntGeq () = verifyBIExprToBool "int" ">="
     [<TestMethod>]
-    member this.IntGreater () = verifyBIExpr "int" ">"
+    member this.IntGreater () = verifyBIExprToBool "int" ">"
     [<TestMethod>]
-    member this.IntNeq () = verifyBIExpr "int" "<>"
+    member this.IntNeq () = verifyBIExprToBool "int" "<>"
     [<TestMethod>]
-    member this.IntNot () = verifyMonoPreExprToBool "int" "!"
+    member this.IntNot () = Assert.ThrowsException(fun _ -> verifyMonoPreExprToBool "int" "!") |> ignore
     [<TestMethod>]
-    member this.IntLength () = verifyMonoPreExprToBool "int" "len"
+    member this.IntLength () = Assert.ThrowsException(fun _ -> verifyMonoPreExprDualTyp "int" "int" "len") |> ignore
     [<TestMethod>]
-    member this.IntDeref () = verifyMonoPostExprToBool "int" "^"
+    member this.IntDeref () = Assert.ThrowsException(fun _ -> verifyMonoPostExprToBool "int" "^") |> ignore
     [<TestMethod>]
     member this.IntAddress () = verifyMonoPreExprDualTyp "int" "^int" "&"
+
+    [<TestMethod>]
+    member this.BoolPlus () = Assert.ThrowsException(fun _ -> verifyBIExpr "bool" "+") |> ignore
+    [<TestMethod>]
+    member this.BoolMinus () = Assert.ThrowsException(fun _ -> verifyBIExpr "bool" "-") |> ignore
+    [<TestMethod>]
+    member this.BoolMult () = Assert.ThrowsException(fun _ -> verifyBIExpr "bool" "*") |> ignore
+    [<TestMethod>]
+    member this.BoolDiv () = Assert.ThrowsException(fun _ -> verifyBIExpr "bool" "/") |> ignore
+    [<TestMethod>]
+    member this.BoolEq () = verifyBIExprToBool "bool" "="
+    [<TestMethod>]
+    member this.BoolLess () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "bool" "<") |> ignore
+    [<TestMethod>]
+    member this.BoolLeq () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "bool" "<=") |> ignore
+    [<TestMethod>]
+    member this.BoolGeq () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "bool" ">=") |> ignore
+    [<TestMethod>]
+    member this.BoolGreater () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "bool" ">") |> ignore
+    [<TestMethod>]
+    member this.BoolNeq () = verifyBIExprToBool "bool" "<>"
+    [<TestMethod>]
+    member this.BoolNot () = verifyMonoPreExprToBool "bool" "!"
+    [<TestMethod>]
+    member this.BoolLength () = Assert.ThrowsException(fun _ -> verifyMonoPreExprDualTyp "bool" "int" "len") |> ignore
+    [<TestMethod>]
+    member this.BoolDeref () = Assert.ThrowsException(fun _ -> verifyMonoPostExprToBool "bool" "^") |> ignore
+    [<TestMethod>]
+    member this.BoolAddress () = verifyMonoPreExprDualTyp "bool" "^bool" "&"
+
+    [<TestMethod>]
+    member this.CharPlus () = Assert.ThrowsException(fun _ -> verifyBIExpr "char" "+") |> ignore
+    [<TestMethod>]
+    member this.CharMinus () = Assert.ThrowsException(fun _ -> verifyBIExpr "char" "-") |> ignore
+    [<TestMethod>]
+    member this.CharMult () = Assert.ThrowsException(fun _ -> verifyBIExpr "char" "*") |> ignore
+    [<TestMethod>]
+    member this.CharDiv () = Assert.ThrowsException(fun _ -> verifyBIExpr "char" "/") |> ignore
+    [<TestMethod>]
+    member this.CharEq () = verifyBIExprToBool "char" "="
+    [<TestMethod>]
+    member this.CharLess () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "char" "<") |> ignore
+    [<TestMethod>]
+    member this.CharLeq () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "char" "<=") |> ignore
+    [<TestMethod>]
+    member this.CharGeq () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "char" ">=") |> ignore
+    [<TestMethod>]
+    member this.CharGreater () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "char" ">") |> ignore
+    [<TestMethod>]
+    member this.CharNeq () = verifyBIExprToBool "char" "<>"
+    [<TestMethod>]
+    member this.CharNot () = Assert.ThrowsException(fun _ -> verifyMonoPreExprToBool "char" "!") |> ignore
+    [<TestMethod>]
+    member this.CharLength () = Assert.ThrowsException(fun _ -> verifyMonoPreExprDualTyp "char" "int" "len") |> ignore
+    [<TestMethod>]
+    member this.CharDeref () = Assert.ThrowsException(fun _ -> verifyMonoPostExprToBool "char" "^") |> ignore
+    [<TestMethod>]
+    member this.CharAddress () = verifyMonoPreExprDualTyp "char" "^char" "&"
+
+    [<TestMethod>]
+    member this.ArrayPlus () = Assert.ThrowsException(fun _ -> verifyBIExpr "int[2]" "+") |> ignore
+    [<TestMethod>]
+    member this.ArrayMinus () = Assert.ThrowsException(fun _ -> verifyBIExpr "int[2]" "-") |> ignore
+    [<TestMethod>]
+    member this.ArrayMult () = Assert.ThrowsException(fun _ -> verifyBIExpr "int[2]" "*") |> ignore
+    [<TestMethod>]
+    member this.ArrayDiv () = Assert.ThrowsException(fun _ -> verifyBIExpr "int[2]" "/") |> ignore
+    [<TestMethod>]
+    member this.ArrayEq () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "int[2]" "=") |> ignore
+    [<TestMethod>]
+    member this.ArrayLess () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "int[2]" "<") |> ignore
+    [<TestMethod>]
+    member this.ArrayLeq () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "int[2]" "<=") |> ignore
+    [<TestMethod>]
+    member this.ArrayGeq () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "int[2]" ">=") |> ignore
+    [<TestMethod>]
+    member this.ArrayGreater () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "int[2]" ">") |> ignore
+    [<TestMethod>]
+    member this.ArrayNeq () = Assert.ThrowsException(fun _ -> verifyBIExprToBool "int[2]" "<>") |> ignore
+    [<TestMethod>]
+    member this.ArrayNot () = Assert.ThrowsException(fun _ -> verifyMonoPreExprToBool "int[2]" "!") |> ignore
+    [<TestMethod>]
+    member this.ArrayLength () = verifyMonoPreExprDualTyp "int[2]" "int" "len"
+    [<TestMethod>]
+    member this.ArrayDeref () = Assert.ThrowsException(fun _ -> verifyMonoPostExprToBool "int[2]" "^") |> ignore
+    [<TestMethod>]
+    member this.ArrayAddress () = verifyMonoPreExprDualTyp "int[2]" "^int[]" "&"
