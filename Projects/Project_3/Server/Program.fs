603f55cce88f99e9973f38c7364510f331ea6e9e
TheAIBot <andreasuranus@hotmail.com>
2020-01-21 21:54:01 +0100

    networking stuff now works. added example to the server

--- Projects/Project_3/Server/Program.fs
+++ Projects/Project_3/Server/Program.fs
@@ -4,5 +4,24 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let receiver = NetworkReceiver(9001)
+    let sender = NetworkSender(9001, IPAddress.Loopback)
+
+    receiver.ReceiveMessageEvent.Add(fun x -> printfn "%s" (System.Text.Encoding.UTF8.GetString(x)))
+
+    receiver.StartListening()
+
+    Async.RunSynchronously (Broadcast(System.Text.Encoding.UTF8.GetBytes("the thing!"), 9001))
+    Async.RunSynchronously (Broadcast(System.Text.Encoding.UTF8.GetBytes("the thing!"), 9001))
+    Async.RunSynchronously (Broadcast(System.Text.Encoding.UTF8.GetBytes("the thing!"), 9001))
+    Async.RunSynchronously (Broadcast(System.Text.Encoding.UTF8.GetBytes("the thing!"), 9001))
+
+
+    Async.RunSynchronously (sender.Send(System.Text.Encoding.UTF8.GetBytes("the taahing!"))) |> ignore
+    Async.RunSynchronously (sender.Send(System.Text.Encoding.UTF8.GetBytes("the taahing!"))) |> ignore
+    Async.RunSynchronously (sender.Send(System.Text.Encoding.UTF8.GetBytes("the taahing!"))) |> ignore
+    Async.RunSynchronously (sender.Send(System.Text.Encoding.UTF8.GetBytes("the taahing!"))) |> ignore
+
+    Async.RunSynchronously (Async.Sleep(3000))
+
     0 // return an integer exit code
--- Projects/Project_3/SharedTypes/Networking.fs
+++ Projects/Project_3/SharedTypes/Networking.fs
@@ -1,16 +1,17 @@
 namespace SharedTypes
 
+open System
 open System.Net
 open System.Net.Sockets
 open System.Net.NetworkInformation;
-open System
+open System.Threading
 
 module NetworkStuff =
-    let convertToBroadcast (uni:IPAddress) (sub:IPAddress) =
+    let private convertToBroadcast (uni:IPAddress) (sub:IPAddress) =
         let broadcastBytes = Array.map2(fun x y -> x ||| ~~~y) (uni.GetAddressBytes()) (sub.GetAddressBytes())
         IPAddress(broadcastBytes)
 
-    let getBroadcastAddresses: IPAddress array =
+    let private getBroadcastAddresses: IPAddress array =
         Array.collect(fun (x: NetworkInterface) -> 
             if not x.IsReceiveOnly && x.NetworkInterfaceType <> NetworkInterfaceType.Loopback && x.OperationalStatus = OperationalStatus.Up
             then // In order to unpack the unique collection from x.GetIPProperties().UnicastAddresses
@@ -22,38 +23,60 @@ module NetworkStuff =
                  bList.ToArray()              
             else Array.empty) (NetworkInterface.GetAllNetworkInterfaces())
 
-    type NetworkSender(port: int, recAddr: IPAddress) =
+    let public Broadcast(bytes, port) =
+        async {               
+            Array.iter(fun (x:IPAddress) ->
+                use broadcastClient = new UdpClient()
+                broadcastClient.ExclusiveAddressUse <- false
+                broadcastClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true) 
+                broadcastClient.EnableBroadcast <- true
+
+                broadcastClient.Connect(IPEndPoint(x, port))
+                broadcastClient.Send(bytes, bytes.Length) |> ignore
+                ) getBroadcastAddresses
+        }    
+
+
+    type public NetworkSender(port: int, recAddr: IPAddress) =
         let port = port
         let receiverAddress = recAddr
-        let client = new UdpClient(port)
-        let mutable broadcastAddresses = Array.empty
-        let getBroadcastAddresses =
-            if broadcastAddresses.Length = 0
-            then broadcastAddresses <- getBroadcastAddresses
-            broadcastAddresses
+        let client = new UdpClient()
+        do
+            client.ExclusiveAddressUse <- false
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true)
+            client.Connect(IPEndPoint(receiverAddress, port))
 
-        member this.Broadcast(bytes) =
-            Array.map(fun (x:IPAddress) ->
-                client.Client.Bind(IPEndPoint(x, port))
-                client.SendAsync(bytes, bytes.Length)
-                ) getBroadcastAddresses
+        member public this.Send(bytes) =
+            async { client.SendAsync(bytes, bytes.Length) |> ignore }
 
-        member this.Send(bytes) =
-            client.Client.Bind(IPEndPoint(receiverAddress, port))
-            client.SendAsync(bytes, bytes.Length)
-
-    type NetworkReceiver(port: int) =
+    type public NetworkReceiver(port: int) =
         let port = port
         let listener = new UdpClient()
-        let receiveEvent = new Event<_>()
-
-        member this.StartListening() =
-            
+        let receiveEvent = new Event<byte array>()
+        do
+            listener.ExclusiveAddressUse <- false
+            listener.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true)
+            listener.Client.Bind(IPEndPoint(IPAddress.Any, port))
 
         [<CLIEvent>]
-        member this.ReceiveMessageEvent = receiveEvent.Publish
+        member public this.ReceiveMessageEvent = receiveEvent.Publish 
 
-        member this.TestEvent(arg) =
-            event1.Trigger(this, arg)            
+        member public this.StartListening() =
+            listener.BeginReceive(AsyncCallback(this.Receive), null) |> ignore
+
+        member private this.Receive (result: IAsyncResult) =
+
+            //get message
+            let endPoint = IPEndPoint(IPAddress.Any, port)
+            let message = listener.EndReceive(result, ref endPoint)
+
+            //start listening again
+            listener.BeginReceive(AsyncCallback(this.Receive), null) |> ignore
+
+            //make a receive event.
+            //This method may be executed at the same time from multiple threads
+            //so it's necessary to synchronize the execution of the events
+            //as that's what the rest of the code would expect
+            lock listener (fun () -> receiveEvent.Trigger(message))
 
 
