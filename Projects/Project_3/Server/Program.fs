db8896300d8cc99f1c521c13492c4070c27323a6
mariya <mariya.tacheva@gmx.com>
2020-01-21 13:03:43 +0100

    ok, async state machine loop works

--- Projects/Project_3/Server/Program.fs
+++ Projects/Project_3/Server/Program.fs
@@ -2,7 +2,25 @@
 
 open System
 
+/// Automaton for the Server, hosting a ping-pong game
+let rec start() = 
+    async {printfn "start"; return! waitingForPlayers()}    
+
+and waitingForPlayers() = 
+    async {printfn "waitingForPlayers"; return! playGame()}    
+
+and playGame() = 
+    async {printfn "playGame"; return! sendNewState()}
+
+and sendNewState() = 
+    async {printfn "sendNewState"; return! waitForClientInput()}   
+
+and waitForClientInput() = 
+    async {printfn "waitForClientInput"; return! sendNewState()}    
+
+/// call "Start" to launch a new Server 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Main"
+    Async.StartImmediate (start())
     0 // return an integer exit code
