63ee268c94a7530f4bd25ade30ab030ba3a0b511
mariya <mariya.tacheva@gmx.com>
2020-01-22 09:33:23 +0100

    comments

--- Projects/Project_3/Client/Library.fs
+++ Projects/Project_3/Client/Library.fs
@@ -1,5 +1,13 @@
 ﻿namespace Client
 
+
+/// Show "Host game | Join game"
+/// "Host game" -> launch Server
+/// "Join game" -> get available servers; connect to 1;
+/// "Start game" -> 
+///     send input periodically (if nothing captured -> empty input); 
+///     receive game state
+/// "Leave game"  
 module Say =
     let hello name =
         printfn "Hello %s" name
