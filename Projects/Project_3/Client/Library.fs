19c29690066650def2eae2fa8912beb6f2e0b04c
mariya <mariya.tacheva@gmx.com>
2020-01-22 10:09:37 +0100

    state machine skeleton (missing "Leaving" state)

--- Projects/Project_3/Client/Library.fs
+++ Projects/Project_3/Client/Library.fs
@@ -1,6 +1,11 @@
 ﻿namespace Client
 
-open SharedTypes.SharedTypes
+open System
+open System.Net
+open System.Threading
+
+open SharedTypes.NetworkStuff
+open SharedTypes.SharedTypes 
 
 module ClientStuff =
 
@@ -19,3 +24,68 @@ module ClientStuff =
 
         member public this.KeyPressed(key: char) =
             "something" |> ignore                  
+
+
+/// Automaton for the Client, connection to a ping-pong game
+module StateMachine = 
+    //TODO: for now, event queue stores strings
+    let ev = AsyncEventQueue<String>()
+    
+    // start in a lobby
+    let rec start() = 
+        async {
+            //TODO: add broadcast message 
+
+            let! msg = ev.Receive();
+            match msg with
+             | "Host game"  -> return! startServerProcess()
+             | "Join game"  -> return! joinGame()
+             | _         -> failwith("waitingForPlayers: unexpected message")
+            }
+
+    /// start a server process to host a game
+    and startServerProcess() = 
+        async {
+            //printfn "state: startServerProcess"; 
+            //TODO: start a server process
+
+            let! msg = ev.Receive();
+            match msg with
+             | "Two players have now joined"  -> return! joinGame()
+             | _         -> failwith("startServerProcess: unexpected message")
+            }
+            
+    /// shows game window; waits for two players to join
+    and joinGame() = 
+        async {
+            //printfn "state: joinGame"; 
+            let! msg = ev.Receive();
+            match msg with
+             | "Two players have now joined"  -> return! receiveGameState()
+             | _         -> failwith("joinGame: unexpected message")
+            }
+
+    /// receives a game state and updates UI
+    and receiveGameState() = 
+        async {
+            //printfn "state: joinGame"; 
+            let! msg = ev.Receive();
+            match msg with
+             | "GameState"  ->
+                            //TODO: send message to update UI
+                            return! sendInput() //TODO: should be periodical
+             | _         -> failwith("receiveGameState: unexpected message")
+            }
+
+    /// receives a key stroke and updates server
+    and sendInput() = 
+        async {
+            //printfn "state: joinGame"; 
+            let! msg = ev.Receive();
+            match msg with
+             | "Up"  ->
+                            //TODO: send input to Server
+                            return! receiveGameState()
+             | _         -> failwith("sendInput: unexpected message")
+            }        
+
