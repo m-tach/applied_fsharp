417e7b76b06319a1b74f920cba5ca27ddbf44829
TheAIBot <andreasuranus@hotmail.com>
2020-01-22 12:37:45 +0100

    remove outdated code

--- Projects/Project_3/Server/Program.fs
+++ Projects/Project_3/Server/Program.fs
@@ -6,24 +6,4 @@ open SharedTypes.NetworkStuff
 
 [<EntryPoint>]
 let main argv =
-    let receiver = NetworkReceiver(9001)
-    let sender = NetworkSender(9001, IPAddress.Loopback)
-
-    receiver.ReceiveMessageEvent.Add(fun x -> printfn "%s" (System.Text.Encoding.UTF8.GetString(x)))
-
-    receiver.StartListening()
-
-    Async.RunSynchronously (Broadcast(System.Text.Encoding.UTF8.GetBytes("the thing!"), 9001))
-    Async.RunSynchronously (Broadcast(System.Text.Encoding.UTF8.GetBytes("the thing!"), 9001))
-    Async.RunSynchronously (Broadcast(System.Text.Encoding.UTF8.GetBytes("the thing!"), 9001))
-    Async.RunSynchronously (Broadcast(System.Text.Encoding.UTF8.GetBytes("the thing!"), 9001))
-
-
-    Async.RunSynchronously (sender.Send(System.Text.Encoding.UTF8.GetBytes("the taahing!"))) |> ignore
-    Async.RunSynchronously (sender.Send(System.Text.Encoding.UTF8.GetBytes("the taahing!"))) |> ignore
-    Async.RunSynchronously (sender.Send(System.Text.Encoding.UTF8.GetBytes("the taahing!"))) |> ignore
-    Async.RunSynchronously (sender.Send(System.Text.Encoding.UTF8.GetBytes("the taahing!"))) |> ignore
-
-    Async.RunSynchronously (Async.Sleep(3000))
-
     0 // return an integer exit code
