b8675085b02d672fe66e7ba15c05a6c68639a5aa
Andreas <bloxxer9001@gmail.com>
2020-01-22 12:31:45 +0100

    More code for navigating pages. Setup of the game screen.

--- Projects/Project_3/FrontendWpf/FrontendWpf/MainWindow.xaml.cs
+++ Projects/Project_3/FrontendWpf/FrontendWpf/MainWindow.xaml.cs
@@ -36,11 +36,26 @@ namespace FrontendWpf
 			screen.Visibility = Visibility.Visible;
 		}
 
-		private void SetLobbyGames(GameServer[] games)
+		// Shorthand, in case we need to do more in the future.
+		// Sets the list of games to show on the lobby list.
+		private void SetLobbyGames(GameServer[] games) => GameServerList.ItemsSource = games;
+
+		// Render the game given a state.
+		public void RenderGame(GameState state)
 		{
-			GameServerList.ItemsSource = games;
+			int middleY = 180;
+			int middleX = 387;
+
+			GameCanvasPlayer1.Y1 = middleY + state.Player1.Position.Y * 10;
+			GameCanvasPlayer1.Y2 = middleY + state.Player1.Position.Y * 10 + 80;
+
+			GameCanvasPlayer2.Y1 = middleY + state.Player2.Position.Y * 10;
+			GameCanvasPlayer2.Y2 = middleY + state.Player2.Position.Y * 10 + 80;
+
+			GameCanvasBall.Margin = new Thickness(middleX + state.Ball.BallPosition.X * 10, middleY + state.Ball.BallPosition.Y * 10, 0, 0);
 		}
 
+		// When the window has loaded, ping for game hosts and get 'em.
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
 			SetLobbyGames(new GameServer[] {
@@ -48,18 +63,33 @@ namespace FrontendWpf
 				new GameServer("World Hello", System.Net.IPAddress.None),
 				new GameServer("Hello Hello", System.Net.IPAddress.None)
 			});
+			RenderGame(new GameState()
+			{
+				Ball = new Ball() { BallPosition = new Vector(15, 0), BallDirection = new Vector(1, 0.25f) },
+				Player1 = new PlayerData() { Position = new Vector(0, 4), Score = 4 },
+				Player2 = new PlayerData() { Position = new Vector(0, 3), Score = 1 }
+			});
 		}
 
+		// Lobby -> Create game
 		private void HostGame_Click(object sender, RoutedEventArgs e) => SetScreen(ScreenCreateGame);
-
+		// Create game -> Lobby
 		private void ExitCreateGame_Click(object sender, RoutedEventArgs e) => SetScreen(ScreenLobby);
 
+		// Join a game host. Currently this prints the dummy IP address and switches the view to the game screen.
 		private void JoinGameBtn_Click(object sender, RoutedEventArgs e)
 		{
-			System.Diagnostics.Debug.Print(e.Source + ";; " + e.RoutedEvent + ";; " + e.OriginalSource);
+			Button btn = e.Source as Button;
+			System.Diagnostics.Debug.Print(btn.Tag + "");
+
+			SetScreen(ScreenGame);
 		}
+
+		// Exit a game screen. Brings you to the lobby.
+		private void ExitGameBtn_Click(object sender, RoutedEventArgs e) => SetScreen(ScreenLobby);
 	}
 
+	// Placeholder, until we tie together the FS modules and this.
 	public class GameServer
 	{
 		public string Name { get; set; }
@@ -70,5 +100,31 @@ namespace FrontendWpf
 			Name = name;
 			IPAddress = ipAddress;
 		}
+	}
+
+	public struct Vector
+	{
+		public float X;
+		public float Y;
+		public Vector(float x, float y) { X = x; Y = y; }
+	}
+
+	public struct PlayerData
+	{
+		public Vector Position;
+		public int Score;
+	}
+
+	public struct Ball
+	{
+		public Vector BallPosition;
+		public Vector BallDirection;
+	}
+
+	public struct GameState
+	{
+		public Ball Ball;
+		public PlayerData Player1;
+		public PlayerData Player2;
 	}
 }
